fc0c000bda4178ae56f48a16b6b92261c7caadde
1hardee <hardishastri24@gmail.com>
2022-12-14 10:54:19 -0500

    final changes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Final-AHMS
+# final-project-ahms
+
+## Team members - 
+- Saurab Prasher - N01547264
+- Anuj Deol - N01550000
+- Hardi Shastri - N01521762
---
+++ js/kids_beauty.js
@@ -0,0 +1,69 @@
+let slideIndex = 0;
+Slides();
+
+function Slides() {
+  let i;
+  let slides = document.getElementsByClassName("Slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(Slides, 2000);
+  // Change image every 1 seconds
+}
+
+function showInput() {
+  if (!email.value == "") {
+    document.getElementById("success_messsage").innerHTML =
+      "Welcome to AHMS community, Now you will get regural news letter and latest promotions, Stay Tuned!! ";
+    document.getElementById("success_messsage").style.color = "green";
+    document.getElementById("success_messsage").style.fontSize = "x-large";
+  }
+}
+
+// window.onload = function () {
+//   document.getElementById('#subs').onclick = validate;
+// };
+// validate();
+
+function validate1() {
+  let email = document.querySelector("#email");
+  let name = document.querySelector("#name");
+  let phonenum = document.querySelector("#phnb");
+
+  let namepattern = /^[a-zA-Z]+ [a-zA-Z]+$/;
+  let emailpattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  let phonenumpattern = /^[0-9]{10}$/;
+
+  //   form validation
+
+  if (email.value == "") {
+    email.nextElementSibling.innerHTML = "Please enter your email";
+  } else if (!emailpattern.test(email.value)) {
+    email.nextElementSibling.innerHTML = "Please enter your correct email ";
+  }
+
+  if (name.value == "") {
+    name.nextElementSibling.innerHTML = "Please enter your full name";
+  } else if (!namepattern.test(name.value)) {
+    name.nextElementSibling.innerHTML = "Please enter your correct name ";
+  }
+
+  if (phonenum.value == "") {
+    phonenum.nextElementSibling.innerHTML = "Please enter your phone number";
+  } else if (!phonenumpattern.test(phonenum.value)) {
+    phonenum.nextElementSibling.innerHTML =
+      "Please enter your correct phone number ";
+  }
+}
+
+document
+  .querySelector(".newspromotionsform")
+  .addEventListener("submit", (e) => {
+    e.preventDefault();
+    validate1();
+  });
